90fd4742b711fe442923ac99061fc2bb4bcc6ac9
Marc Palmer <marc@anyware.co.uk>
2012-03-27 17:41:18 +0100

    We are M1

--- PlatformCoreGrailsPlugin.groovy
+++ PlatformCoreGrailsPlugin.groovy
@@ -25,7 +25,7 @@ import org.springframework.core.io.FileSystemResource
 
 class PlatformCoreGrailsPlugin {
     // the plugin version
-    def version = "1.0.M1-SNAPSHOT"
+    def version = "1.0.M1"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3 > *"
     // the other plugins this plugin depends on
