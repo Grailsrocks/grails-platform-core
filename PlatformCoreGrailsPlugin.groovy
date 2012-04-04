15a5805b17111a3ad0a8d2b2da5a901081da4aff
Stephane Maldini <smaldini@doc4web.com>
2012-04-04 23:21:08 +0200

    upgrade version snapshot

--- PlatformCoreGrailsPlugin.groovy
+++ PlatformCoreGrailsPlugin.groovy
@@ -25,7 +25,7 @@ import org.springframework.core.io.FileSystemResource
 
 class PlatformCoreGrailsPlugin {
     // the plugin version
-    def version = "1.0.M1"
+    def version = "1.0.M2-SNAPSHOT"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3 > *"
     // the other plugins this plugin depends on
