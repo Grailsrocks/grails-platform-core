6370e1fc853916957f367c5c8a386d43b1521cc4
Marc Palmer <marc@anyware.co.uk>
2012-06-29 11:19:34 +0100

    Version bump for snapshot release

--- PlatformCoreGrailsPlugin.groovy
+++ PlatformCoreGrailsPlugin.groovy
@@ -25,7 +25,7 @@ import org.grails.plugin.platform.events.registry.DefaultEventsRegistry
 
 class PlatformCoreGrailsPlugin {
     // the plugin version
-    def version = "1.0.M2h-SNAPSHOT"
+    def version = "1.0.M2i-SNAPSHOT"
 
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3 > *"
