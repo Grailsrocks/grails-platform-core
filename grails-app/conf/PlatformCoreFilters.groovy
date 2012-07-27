8b69b2a5eb1bbb56ccb98165107f9e532e354291
Marc Palmer <marc@anyware.co.uk>
2012-07-27 18:15:51 +0100

    Fix for clash of url mappings with platform-ui

--- grails-app/conf/PlatformCoreFilters.groovy
+++ grails-app/conf/PlatformCoreFilters.groovy
@@ -34,15 +34,17 @@ class PlatformCoreFilters {
         if (Environment.current == Environment.DEVELOPMENT) {
             'platformDev'(uri:'/platform/**'){
                 before = {
-                    def UA = request.getHeader('User-Agent')
-                    // OK need a regex in future... check Lion
-                    def OSX = UA.indexOf('OS X 10_7')
-                    if (OSX == -1) {
-                        // try mountain lion
-                        OSX = UA.indexOf('OS X 10_8')
-                    }
-                    if (OSX > -1) {
-                        redirect(mapping:'platformFancy', controller:'debug', action:actionName, id:params.id)
+                    if (controllerName == 'platformTools') {
+                        def UA = request.getHeader('User-Agent')
+                        // OK need a regex in future... check Lion
+                        def OSX = UA.indexOf('OS X 10_7')
+                        if (OSX == -1) {
+                            // try mountain lion
+                            OSX = UA.indexOf('OS X 10_8')
+                        }
+                        if (OSX > -1) {
+                            redirect(mapping:'platformFancy', action:actionName, id:params.id)
+                        }
                     }
                 }
             }
