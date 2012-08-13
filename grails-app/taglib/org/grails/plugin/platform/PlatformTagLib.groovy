0556973391fae388d1bffeaa6c48fa6d15184f08
Marc Palmer <marc@anyware.co.uk>
2012-08-13 15:17:46 +0100

    Removed r:require in uiOverlay

--- grails-app/taglib/org/grails/plugin/platform/PlatformTagLib.groovy
+++ grails-app/taglib/org/grails/plugin/platform/PlatformTagLib.groovy
@@ -23,8 +23,6 @@ class PlatformTagLib {
     static namespace = "p"
     
     def uiOverlay = { attrs, body ->
-        out << r.require(module:'plugin.platformCore.tools')
-        
         out << g.render(plugin:'platformCore', template:'/platformTools/uiOverlay', model:[bodyContent:body])
     }
     
