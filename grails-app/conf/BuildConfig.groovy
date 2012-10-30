3e0added5eb3cef512943ff61d98917f43d4b126
Stephane Maldini <smaldini@doc4web.com>
2012-10-30 13:44:44 +0000

    revert to 2.1.1

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -14,7 +14,7 @@ grails.project.dependency.resolution = {
     repositories {
         grailsCentral()
         grailsRepo "http://grails.org/plugins"
-        
+
         // uncomment the below to enable remote dependency resolution
         // from public Maven repositories
         //mavenCentral()
