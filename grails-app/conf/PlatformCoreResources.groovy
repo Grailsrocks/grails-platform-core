3455e9fd20fe5e8425b8cc16763358bb0b3cf8d2
Marc Palmer <marc@anyware.co.uk>
2012-11-30 22:16:07 +0000

    Added local dev jquery for dev UI

--- grails-app/conf/PlatformCoreResources.groovy
+++ grails-app/conf/PlatformCoreResources.groovy
@@ -1,6 +1,7 @@
 modules = {
     'plugin.platformCore.tools' {
         resource url:[plugin:'platformCore', dir:'css', file:'bootstrap.min.css']
+        resource url:[plugin:'platformCore', dir:'js', file:'jquery.1.8.3.min.js']
         resource url:[plugin:'platformCore', dir:'js', file:'bootstrap.min.js']
         resource url:[plugin:'platformCore', dir:'css', file:'platformTools.css']
     }
