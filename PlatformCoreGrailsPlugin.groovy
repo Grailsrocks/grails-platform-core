31e154fe4e643818b8b3e72d216fec2ec92c7d4e
Marc Palmer <marc@anyware.co.uk>
2013-01-16 15:06:27 +0000

    RC5 release

--- PlatformCoreGrailsPlugin.groovy
+++ PlatformCoreGrailsPlugin.groovy
@@ -24,7 +24,7 @@ import org.grails.plugin.platform.events.registry.DefaultEventsRegistry
 
 class PlatformCoreGrailsPlugin {
     // the plugin version
-    def version = "1.0.RC4"
+    def version = "1.0.RC5"
 
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3 > *"
